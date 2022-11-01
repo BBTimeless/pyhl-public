2828453298d9561e47df305a3718218997530934
Brian Breeden <breedenb@gmail.com>
2022-11-01 09:40:12 -0400

    functions added to fetch skater data by team and skater ids

--- skaters.py
+++ skaters.py
@@ -2,5 +2,20 @@ import pandas as pd
 
 DATA = pd.read_csv('static/data/data_skater.csv')
 
-def get_skater_data():
-    return(DATA)
+'''
+Fetches skater data from the CSV file via a list of skater ids. The data is then returned in a list.
+'''
+def get_skater_data_by_id(player_ids):
+    skaters = DATA.loc[DATA['id'].isin(player_ids)]
+    return(skaters)
+
+'''
+Fetches skater data from the CSV file via a list of team ids. The data is then returned in a list.
+'''
+def get_skater_data_by_team_id(team_ids):
+    skaters = DATA.loc[DATA['currentteamid'].isin(team_ids)]
+    return(skaters)
+
+
+get_skater_data_by_id([8476292, 8474090, 8476462])
+get_skater_data_by_team_id([5, 4, 1])
