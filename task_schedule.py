fcafb9c8d8f8c79fcd9c6fd342df1268846f7473
Brian Breeden <breedenb@gmail.com>
2022-11-04 18:39:14 -0400

    datetime converted to EST

--- task_schedule.py
+++ task_schedule.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import pandas as pd
+from datetime import datetime, timedelta
 
 '''
 Calls the api and formats a list of dictionaries containing the game schedule.
@@ -28,8 +29,16 @@ def write_game_schedule():
     df = pd.DataFrame(get_game_schedule())
     df.to_csv('static/data/data_schedule.csv', encoding='utf-8', index=False)
 
+'''
+Helper function to format the date string into an easier to read format.
+'''
 def format_date(date_string):
-    # %Z    UTC
     dt_string_formatted = date_string.replace('T', ' ')
-    dt_string_formatted = dt_string_formatted.replace('Z',' UTC')
-    return(dt_string_formatted)
+    dt_string_formatted = dt_string_formatted.replace('Z', ' UTC')
+
+    dt_obj = datetime.strptime(dt_string_formatted, '%Y-%m-%d %H:%M:%S %Z')
+    est = dt_obj - timedelta(hours=4)
+
+    final_db_dt_entry = est.strftime('%Y-%m-%d %H:%M:%S')
+
+    return(final_db_dt_entry)
